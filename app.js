62a98081f298fcea9fd826adbe96c5b7ad14725c
kevinorsinet <kevin.orsinet@gmail.com>
2021-12-15 13:57:20 +0100

    afficher les contenus dans le tableau

--- app.js
+++ app.js
@@ -39,12 +39,10 @@
 
     // Sauvegarde dans le local storage
     localStorage.setItem('products', JSON.stringify(productsList));
-
-    
   }
 
   function getProducts() {
-    return localStorage.getItem('products');
+    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
 
   }
 
@@ -56,10 +54,20 @@
       let row = tableContent.insertRow();
       let id = row.insertCell(0);
       let name = row.insertCell(1);
+      let category = row.insertCell(2);
+      let price = row.insertCell(3);
+      let quantity = row.insertCell(4);
+      let action = row.insertCell(5);
 
-      id.innerHTML = 1;
-      name.innerHTML = "Product 1"; 
+      id.innerHTML = item.idProduct;
+      name.innerHTML = item.nameProduct; 
+      category.innerHTML = item.categoryProduct;
+      price.innerHTML = `${item.priceProduct} €`; 
+      quantity.innerHTML = item.quantityProduct; 
+      action.innerHTML = `
+      <a><i class="fas fa-edit iconEdit"></i></a>
+      <a><i class="fas fa-trash-alt iconDelete"></i></a>`;
       
-    })
-  })()
+    });
+  })();
 
